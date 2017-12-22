1fae7d8245d4fc8df770a1cc0c26317c3bff335b
huseyingokdag <hsyngkdg@gmail.com>
2017-12-23 00:10:51 +0300

    Test.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <b-alert variant="info" show>
-        //TODO
-    </b-alert>
-  </div>
+<div>
+  <b-alert variant="info" show>
+    //TODO
+  </b-alert>
+  <!-- test -->
+</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-    }
+  data() {
+    return {}
   }
 }
 </script>
